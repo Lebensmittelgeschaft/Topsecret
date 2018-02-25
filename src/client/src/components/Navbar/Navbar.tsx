56ffa9d910cf7343de27442ff5df45d9e54ba232
Shaked Manes <maneshaked@gmail.com>
2018-02-25 16:47:37 +0200

    Delete some devDependencies

---
+++ src/client/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import AppBar from 'material-ui/AppBar';
+import Tabs, { Tab } from 'material-ui/Tabs';
+
+export interface NavbarProps {
+
+}
+
+const navbar = (props: NavbarProps) => {
+  return (
+    <AppBar position="static">
+    </AppBar>
+  );
+}
+
+export default navbar;
