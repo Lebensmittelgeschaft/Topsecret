d7ff8760df4ace842b11c5a90d0e1f711350a477
Shaked Manes <maneshaked@gmail.com>
2018-02-26 15:18:42 +0200

    Starting develop navbar for client

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -2,15 +2,46 @@ import * as React from 'react';
 import AppBar from 'material-ui/AppBar';
 import Tabs, { Tab } from 'material-ui/Tabs';
 
+export const NAVBAR_TAB_FEED = 'Feed';
+export const NAVBAR_TAB_CHAT = 'Chat';
+export const NAVBAR_TAB_RANDOMCHAT = 'RandomChat';
+export const NAVBAR_TAB_SETTINGS = 'Settings';
+
+const NAVBAR_TABS = [NAVBAR_TAB_FEED, NAVBAR_TAB_CHAT, NAVBAR_TAB_RANDOMCHAT, NAVBAR_TAB_SETTINGS];
+
 export interface NavbarProps {
 
 }
 
-const navbar = (props: NavbarProps) => {
-  return (
-    <AppBar position="static">
-    </AppBar>
-  );
+export interface NavbarState {
+  current: string;
 }
 
-export default navbar;
+class Navbar extends React.Component<NavbarProps, NavbarState> {
+
+  state = {
+    current: NAVBAR_TAB_FEED
+  };
+
+  changeHanlder = (event: object, value: string) => {
+    this.setState({ current: value });
+  }
+
+  render() {    
+    const tabs: JSX.Element[] = [];
+    for (const tab of NAVBAR_TABS) {
+      tabs.push(<Tab value={tab} label={tab}/>);
+    }
+
+    return (
+      <AppBar position="static">
+        <Tabs value={this.state.current} onChange={this.changeHanlder}>
+           {tabs}
+        </Tabs>
+      </AppBar>
+    );
+  }
+
+}
+
+export default Navbar;
